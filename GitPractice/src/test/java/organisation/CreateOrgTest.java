4bdb9882969e05c7854b33752b6952f04d17e4ae
Lakshmi19496 <164867386+Lakshmi19496@users.noreply.github.com>
2024-08-13 14:32:36 +0530

    Update CreateOrgTest.java

--- GitPractice/src/test/java/organisation/CreateOrgTest.java
+++ GitPractice/src/test/java/organisation/CreateOrgTest.java
@@ -4,6 +4,7 @@ public class CreateOrgTest {
 	public static void main(String[] args) {
 		System.out.println("====created org=====");
 		System.out.println("====created org=====");
+		System.out.println("====created org=====");
 	}
 
 }
