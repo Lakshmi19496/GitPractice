c5fc24d884af8c1234d555eac3d71c590e6d8af3
lenovo <lenovo@DESKTOP-PDHRC70>
2024-08-13 14:27:05 +0530

    changed org

--- GitPractice/src/test/java/organisation/CreateOrgTest.java
+++ GitPractice/src/test/java/organisation/CreateOrgTest.java
@@ -3,6 +3,7 @@ package organisation;
 public class CreateOrgTest {
 	public static void main(String[] args) {
 		System.out.println("====created org=====");
+		System.out.println("====created org=====");
 	}
 
 }
